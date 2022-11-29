d40b70c1ddcad2d3b2c7be769003a13c23b97096
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2022-11-29 16:04:11 +0300

    Create Item class and add properties

---
+++ classes/item.rb
@@ -0,0 +1,20 @@
+class Item
+    attr_reader :id;
+    attr_accessor :genre, :publish_date, :source, :author
+    def initialize(publish_date)
+        @id=random.rand(1...1000)
+        @genre=nil
+        @author=nil
+        @source=nil
+        @archieved=false
+        @publish_date=publish_date
+    end
+    def can_be_achieved?
+        archived_date = Date.iso8601(@publish_date).next_year(10)
+        Date.today > archived_date
+    end
+
+    def move_to_archive()
+        @archieved=can_be_achieved
+    end
+end
