700b5b7f25137729e7297ca16ff8aff2b704ef99
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2022-11-30 17:32:52 +0300

    Create can be achieved and the properties.

--- classes/game.rb
+++ classes/game.rb
@@ -1,11 +1,15 @@
-require './item'
-class Game< Item
-    def initialize(last_played_date, multiplayer, published_date)
-        super(publish_date)
-        @last_played_date=last_played_date
-        @multiplayer=multiplayer
-    end
-    def can_be_archived?
-        Date.today > Date.iso8601(@last_played_at).next_year(2) || super
-      end
+require_relative 'item'
+class Game < Item
+  attr_accessor :last_played_at, :multiplayer
+  attr_reader :publish_date
+
+  def initialize(last_played_at, multiplayer, publish_date)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    Date.today > Date.iso8601(@last_played_at).next_year(2) || super
+  end
 end
