6066153d3e0dafa0840176db54325c010c829945
ZikaZaki <zika.github@gmail.com>
2022-12-03 01:43:06 +0300

    Add schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,94 @@
 /** Create the database */
-CREATE TABLE book (
-    id SERIAL PRIMARY KEY,
-	publisher VARCHAR(100) ,
-    cover_state VARCHAR(100)
-);
+CREATE DATABASE IF NOT EXISTS `catalog_of_things` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE TABLE label (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(100),
-    color VARCHAR(100)
-);
+CREATE TABLE Genres (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(250) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE Authors (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(250) NOT NULL,
+    last_name VARCHAR(250) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/** Create the tables */
-CREATE TABLE games(
-    id INT GENERATED ALWAYS AS IDENTITY,
-    last_played_date DATE,
-    published_date DATE,
-    multiplayer BOOLEAN,
-    PRIMARY KEY(id)
-);
-CREATE TABLE authors(
-    id INT GENERATED ALWAYS AS IDENTITY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    PRIMARY KEY(id)
-);
+CREATE TABLE Labels (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(250) NOT NULL,
+    color VARCHAR(250) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE Sources (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(250) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE Books (
+    id INT NOT NULL AUTO_INCREMENT,
+    publish_date DATE NOT NULL,
+    publisher VARCHAR(250) NOT NULL,
+    cover_state VARCHAR(100) NOT NULL,
+    archived BOOLEAN NOT NULL,
+    genre_id INT NOT NULL,
+    author_id INT NOT NULL,
+    label_id INT NOT NULL,
+    source_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (genre_id) REFERENCES Genres(id),
+    FOREIGN KEY (author_id) REFERENCES Authors(id),
+    FOREIGN KEY (label_id) REFERENCES Labels(id),
+    FOREIGN KEY (source_id) REFERENCES Sources(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE Movies (
+    id INT NOT NULL AUTO_INCREMENT,
+    publish_date DATE NOT NULL,
+    silent BOOLEAN NOT NULL,
+    archived BOOLEAN NOT NULL,
+    genre_id INT NOT NULL,
+    author_id INT NOT NULL,
+    label_id INT NOT NULL,
+    source_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (genre_id) REFERENCES Genres(id),
+    FOREIGN KEY (author_id) REFERENCES Authors(id),
+    FOREIGN KEY (label_id) REFERENCES Labels(id),
+    FOREIGN KEY (source_id) REFERENCES Sources(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE MusicAlbums (
+    id INT NOT NULL AUTO_INCREMENT,
+    publish_date DATE NOT NULL,
+    on_spotify BOOLEAN NOT NULL,
+    archived BOOLEAN NOT NULL,
+    genre_id INT NOT NULL,
+    author_id INT NOT NULL,
+    label_id INT NOT NULL,
+    source_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (genre_id) REFERENCES Genres(id),
+    FOREIGN KEY (author_id) REFERENCES Authors(id),
+    FOREIGN KEY (label_id) REFERENCES Labels(id),
+    FOREIGN KEY (source_id) REFERENCES Sources(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE Games (
+    id INT NOT NULL AUTO_INCREMENT,
+    publish_date DATE NOT NULL,
+    multiplayer BOOLEAN NOT NULL,
+    last_played DATE NOT NULL,
+    archived BOOLEAN NOT NULL,
+    genre_id INT NOT NULL,
+    author_id INT NOT NULL,
+    label_id INT NOT NULL,
+    source_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (genre_id) REFERENCES Genres(id),
+    FOREIGN KEY (author_id) REFERENCES Authors(id),
+    FOREIGN KEY (label_id) REFERENCES Labels(id),
+    FOREIGN KEY (source_id) REFERENCES Sources(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
