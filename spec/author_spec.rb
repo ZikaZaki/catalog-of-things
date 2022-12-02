5a8e17ef773db16dff22bf79568c6e2c94a066ce
ZikaZaki <zika.github@gmail.com>
2022-12-03 01:11:53 +0300

    Fix linter errors

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -2,21 +2,21 @@ require './lib/classes/author'
 require './lib/classes/game'
 
 describe Author do
-    it "has a name" do
-        author = Author.new(first_name: "Zack", last_name: "Ali")
-        expect(author.first_name).to eq("Zack")
-        expect(author.last_name).to eq("Ali")
-    end
+  it 'has a name' do
+    author = Author.new(first_name: 'Zack', last_name: 'Ali')
+    expect(author.first_name).to eq('Zack')
+    expect(author.last_name).to eq('Ali')
+  end
 
-    it "author has a game" do
-        game = Game.new(publish_date: '2020-10-21', multiplayer: true, last_played_at: '2009-10-02')
-        author = Author.new(first_name: "Zack", last_name: "Ali")
-        author.add_item(game)
-        expect(author.items.include?(game)).to eq(true)
-    end
-    
-    it "author is an instance of Author" do
-        author = Author.new(first_name: "Zack", last_name: "Ali")
-        expect(author).to be_an_instance_of(Author)
-    end
+  it 'author has a game' do
+    game = Game.new(publish_date: '2020-10-21', multiplayer: true, last_played_at: '2009-10-02')
+    author = Author.new(first_name: 'Zack', last_name: 'Ali')
+    author.add_item(game)
+    expect(author.items.include?(game)).to eq(true)
+  end
+
+  it 'author is an instance of Author' do
+    author = Author.new(first_name: 'Zack', last_name: 'Ali')
+    expect(author).to be_an_instance_of(Author)
+  end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,23 +2,23 @@ require './lib/classes/label'
 require './lib/classes/book'
 
 describe Book do
-    let(:book) { Book.new(publish_date: '2019-01-01', publisher: 'Zika Press', cover_state: 'good') }
-    
-    it 'should have a publish_date' do
-        expect(book.publish_date.strftime('%Y-%m-%d')).to eq('2019-01-01')
-    end
-    
-    it 'should have an archived status' do
-        expect(book.archived).to eq(false)
-    end
-    
-    it 'should have a label that is equalls nill' do
-        expect(book.label).to eq(nil)
-    end
+  let(:book) { Book.new(publish_date: '2019-01-01', publisher: 'Zika Press', cover_state: 'good') }
 
-    it 'should have a label that is not nill' do
-        label = Label.new(title: 'Final Words', color: 'red')
-        book.label = label
-        expect(book.label).to eq(label)
-    end
+  it 'should have a publish_date' do
+    expect(book.publish_date.strftime('%Y-%m-%d')).to eq('2019-01-01')
+  end
+
+  it 'should have an archived status' do
+    expect(book.archived).to eq(false)
+  end
+
+  it 'should have a label that is equalls nill' do
+    expect(book.label).to eq(nil)
+  end
+
+  it 'should have a label that is not nill' do
+    label = Label.new(title: 'Final Words', color: 'red')
+    book.label = label
+    expect(book.label).to eq(label)
+  end
 end
